7d7ebcce58f550fa4e08d7b1bfc2006d0d142fd8
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-07 23:45:24 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,20 +1,43 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("avatar")
-        .setDescription("Get user's avatar")
+        .setDescription("Get a user's avatar")
         .addUserOption(option => option.setName("target").setDescription("Select a user")),
+
     async execute(context) {
-        const user = context.isPrefix ? (context.message.mentions.users.first() || context.message.author) : (context.interaction.options.getUser("target") || context.interaction.user);
+        // Emojis
+        const arrow = "<:flecha:1414301944868245574>";
+        const blueHeart = "<:blue_heart:1414309560231002194>";
 
+        // User selection
+        const user = context.isPrefix
+            ? (context.message.mentions.users.first() || context.message.author)
+            : (context.interaction.options.getUser("target") || context.interaction.user);
+
+        const avatarURL = user.displayAvatarURL({ dynamic: true, size: 1024 });
+
+        // Embed
         const embed = new EmbedBuilder()
-            .setTitle(`❣️Avatar of ${user.username}`)
-            .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+            .setTitle(`❣️ Avatar of ${user.username}`)
             .setColor("Blue")
-            .setTimestamp();
+            .setImage(avatarURL)
+            .setTimestamp()
+            .setDescription(`${arrow} **Username:** ${user.username}\n${blueHeart} **ID:** ${user.id}`);
 
-        if (context.isPrefix) await context.message.reply({ embeds: [embed] });
-        else await context.interaction.reply({ embeds: [embed] });
+        // Download button
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setLabel("📜 Download Avatar")
+                .setStyle(ButtonStyle.Link)
+                .setURL(avatarURL)
+        );
+
+        if (context.isPrefix) {
+            await context.message.reply({ embeds: [embed], components: [row] });
+        } else {
+            await context.interaction.reply({ embeds: [embed], components: [row] });
+        }
     }
 };
