4ed2c9be892950c17b99776f820fe05791eb28f5
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-07 22:36:05 +0530

    Update and rename server.js to serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,19 +2,22 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("server")
-        .setDescription("Get server information"),
+        .setName("serverinfo")
+        .setDescription("Get detailed server information"),
     async execute(context) {
         const guild = context.isPrefix ? context.message.guild : context.interaction.guild;
 
         const embed = new EmbedBuilder()
             .setTitle(`🏰 ${guild.name} Info`)
             .addFields(
-                { name: "Members", value: `${guild.memberCount}`, inline: true },
                 { name: "Owner", value: `<@${guild.ownerId}>`, inline: true },
-                { name: "Region", value: guild.preferredLocale, inline: true }
+                { name: "Members", value: `${guild.memberCount}`, inline: true },
+                { name: "Boosts", value: `${guild.premiumSubscriptionCount}`, inline: true },
+                { name: "Roles", value: `${guild.roles.cache.size}`, inline: true },
+                { name: "Created On", value: `<t:${Math.floor(guild.createdTimestamp/1000)}:D>`, inline: true }
             )
-            .setColor("Orange")
+            .setColor("Blue")
+            .setThumbnail(guild.iconURL({ dynamic: true }))
             .setTimestamp();
 
         if (context.isPrefix) await context.message.reply({ embeds: [embed] });
