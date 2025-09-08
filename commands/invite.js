35806297d2115ba7833d6cf98dc92fa6f3020cd0
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-08 15:26:15 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,17 +10,24 @@ module.exports = {
         const botName = client.user.tag;
         const clientId = client.user.id;
 
-        // Discord OAuth2 bot invite link with admin permissions (you can adjust permissions if needed)
+        // Discord OAuth2 bot invite link with admin permissions (adjust permissions if needed)
         const inviteLink = `https://discord.com/oauth2/authorize?client_id=${clientId}&scope=bot%20applications.commands&permissions=8`;
 
         const embed = new EmbedBuilder()
             .setTitle(`${blueHeart} Invite ${botName}`)
-            .setDescription(`Click the link below to invite **${botName}** to your server:\n\n[Invite Me](${inviteLink})`)
+            .setDescription(`Click the button below to invite **${botName}** to your server!`)
             .setColor("Blue")
             .setTimestamp()
             .setFooter({ text: `Requested by ${isPrefix ? message.author.tag : interaction.user.tag}` });
 
-        if (isPrefix) message.reply({ embeds: [embed] });
-        else interaction.reply({ embeds: [embed] });
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setLabel("Invite Me")
+                .setStyle(ButtonStyle.Link)
+                .setURL(inviteLink)
+        );
+
+        if (isPrefix) message.reply({ embeds: [embed], components: [row] });
+        else interaction.reply({ embeds: [embed], components: [row] });
     }
 };
