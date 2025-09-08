e1110054d34fd80aaac6fcafcfff9e13aed793d0
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-08 10:27:15 +0530

    Update banner.js

--- commands/banner.js
+++ commands/banner.js
@@ -1,31 +1,32 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const heart = "<a:blue_heart:1414309560231002194>";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("banner")
-        .setDescription("Get the profile banner of a user")
-        .addUserOption(option => option.setName("target").setDescription("Select a user")),
+        .setDescription("Get the banner of a user")
+        .addUserOption(option =>
+            option.setName("target").setDescription("Select a user")
+        ),
 
-    async execute({ message, interaction, isPrefix, client }) {
-        const blueHeart = "<a:blue_heart:1414309560231002194>";
-
+    async execute({ message, interaction, isPrefix }) {
         const user = isPrefix
             ? (message.mentions.users.first() || message.author)
             : (interaction.options.getUser("target") || interaction.user);
 
-        // Fetch full user (to get banner)
-        const fetchedUser = await client.users.fetch(user.id, { force: true });
+        // 🔹 Fetch the full user profile to access banner
+        const fetchedUser = await user.fetch(true);
 
         if (!fetchedUser.banner) {
-            const replyMsg = `❌ ${user.tag} does not have a profile banner set.`;
+            const replyMsg = `${heart} ${user.tag} has no banner set!`;
             if (isPrefix) return message.reply(replyMsg);
             else return interaction.reply({ content: replyMsg, ephemeral: true });
         }
 
-        const bannerURL = fetchedUser.bannerURL({ dynamic: true, size: 1024 });
+        const bannerURL = fetchedUser.bannerURL({ dynamic: true, size: 2048 });
 
         const embed = new EmbedBuilder()
-            .setTitle(`${blueHeart} Banner of ${user.tag}`)
+            .setTitle(`${heart} Banner of ${user.tag}`)
             .setImage(bannerURL)
             .setColor("Blue")
             .setTimestamp();
