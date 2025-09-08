bfc74f326c8e6c35a4b4781b8ede7cec7a82ff8b
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-08 18:32:38 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,16 +4,23 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("info")
         .setDescription("Get bot info"),
+
     async execute(context) {
+        const blueHeart = "<a:blue_heart_1414309560231002194:1414309560231002194>";
+        const userTag = context.isPrefix ? context.message.author.tag : context.interaction.user.tag;
+        const userMention = context.isPrefix ? `<@${context.message.author.id}>` : `<@${context.interaction.user.id}>`;
+        const serverName = context.isPrefix ? context.message.guild.name : context.interaction.guild.name;
+
         const embed = new EmbedBuilder()
-            .setTitle("🤖 Bot Information")
+            .setTitle(`🤖 Legendary Bot Info ${blueHeart}`)
+            .setDescription(`Greetings ${userMention}!\n\nI am a mighty and legendary bot, designed to make your server an epic place to be. From moderating chaos to enhancing fun, I handle it all with style and power. ${blueHeart}\n\nSpecial thanks to **${serverName}** for hosting me!`)
             .addFields(
-                { name: "Developer", value: "👑JEETENDRA❤" },
-                { name: "Library", value: "Discord.js v14" },
-                { name: "Status", value: "Active" }
+                { name: "Developer", value: "👑 JEETENDRA ❤", inline: true },
+                { name: "Library", value: "Discord.js v14", inline: true },
+                { name: "Status", value: "⚡ Active and Legendary", inline: true }
             )
             .setColor("Blue")
-            .setFooter({ text: "🤝FEEL THE HONOR !" })
+            .setFooter({ text: `🤝 Serving with honor for ${serverName}` })
             .setTimestamp();
 
         if (context.isPrefix) {
