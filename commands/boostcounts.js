0b1d1ee4965639ba481841a44f8178fedfe3ff9d
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-08 10:37:29 +0530

    Update boostcounts.js

--- commands/boostcounts.js
+++ commands/boostcounts.js
@@ -1,11 +1,31 @@
-// Boosts info
-const boostersList = guild.members.cache
-    .filter(m => m.premiumSince)
-    .map(m => `<a:blue_heart:1414309560231002194> <@${m.id}>`)
-    .join("\n") || "No one boosted this guild";
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const heart = "<a:blue_heart:1414309560231002194>";
 
-embed.addFields(
-    { name: '__Total Boosts__', value: `${heart} ${totalBoosts}`, inline: true },
-    { name: '__Boost Level__', value: `${heart} ${boostLevel}`, inline: true },
-    { name: '__Boosters__', value: boostersList, inline: false }
-);
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("boostcount")
+        .setDescription("Show total server boosts"),
+    async execute(context) {
+        const guild = context.isPrefix ? context.message.guild : context.interaction.guild;
+
+        const totalBoosts = guild.premiumSubscriptionCount;
+        const boostLevel = guild.premiumTier;
+        const boostersList = guild.members.cache
+            .filter(m => m.premiumSince)
+            .map(m => `${heart} <@${m.id}>`)
+            .join("\n") || "No boosters yet";
+
+        const embed = new EmbedBuilder()
+            .setTitle(`🚀 ${guild.name} Boost Info`)
+            .setColor("Blue")
+            .setTimestamp()
+            .addFields(
+                { name: '__Total Boosts__', value: `${heart} ${totalBoosts}`, inline: true },
+                { name: '__Boost Level__', value: `${heart} ${boostLevel}`, inline: true },
+                { name: '__Boosters__', value: boostersList, inline: false }
+            );
+
+        if (context.isPrefix) await context.message.reply({ embeds: [embed] });
+        else await context.interaction.reply({ embeds: [embed] });
+    }
+};
