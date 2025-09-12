2bd84354109a41b67568c58e6265b58bfd1ef7e0
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-12 21:48:24 +0530

    Update storage.js

--- utils/storage.js
+++ utils/storage.js
@@ -3,25 +3,31 @@ const fs = require("fs");
 // Default prefix
 const defaultPrefix = "!";
 
+// Safe JSON loader
+function safeRead(file, fallback = {}) {
+    try {
+        return JSON.parse(fs.readFileSync(file, "utf8") || "{}");
+    } catch {
+        return fallback;
+    }
+}
+
 // Prefixes
 const prefixFile = "./prefixes.json";
 if (!fs.existsSync(prefixFile)) fs.writeFileSync(prefixFile, "{}");
-const getPrefixes = () => JSON.parse(fs.readFileSync(prefixFile, "utf8"));
+const getPrefixes = () => safeRead(prefixFile, {});
 const savePrefixes = (prefixes) => fs.writeFileSync(prefixFile, JSON.stringify(prefixes, null, 4));
 
 // Blocked users
 const blockFile = "./block.json";
 if (!fs.existsSync(blockFile)) fs.writeFileSync(blockFile, "{}");
-const getBlocked = () => JSON.parse(fs.readFileSync(blockFile, "utf8"));
+const getBlocked = () => safeRead(blockFile, {});
 const saveBlocked = (data) => fs.writeFileSync(blockFile, JSON.stringify(data, null, 4));
 
 // Autorole
 const autoroleFile = "./autorole.json";
 if (!fs.existsSync(autoroleFile)) fs.writeFileSync(autoroleFile, "{}");
-const getAutorole = () => {
-    try { return JSON.parse(fs.readFileSync(autoroleFile, "utf8") || "{}"); }
-    catch { return {}; }
-};
+const getAutorole = () => safeRead(autoroleFile, {});
 const saveAutorole = (data) => fs.writeFileSync(autoroleFile, JSON.stringify(data, null, 4));
 
 module.exports = { defaultPrefix, getPrefixes, savePrefixes, getBlocked, saveBlocked, getAutorole, saveAutorole };
