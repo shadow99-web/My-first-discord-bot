cd36d3b30baa9bf4f06a59526e2ad9f171ff15fd
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-12 18:55:37 +0530

    Create storage.js

---
+++ utils/storage.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+// Prefix
+const defaultPrefix = "!";
+const prefixFile = "./prefixes.json";
+if (!fs.existsSync(prefixFile)) fs.writeFileSync(prefixFile, "{}");
+const getPrefixes = () => JSON.parse(fs.readFileSync(prefixFile, "utf8"));
+const savePrefixes = (prefixes) => fs.writeFileSync(prefixFile, JSON.stringify(prefixes, null, 4));
+
+// Blocked Users
+const blockFile = "./block.json";
+if (!fs.existsSync(blockFile)) fs.writeFileSync(blockFile, "{}");
+const getBlocked = () => JSON.parse(fs.readFileSync(blockFile, "utf8"));
+const saveBlocked = (data) => fs.writeFileSync(blockFile, JSON.stringify(data, null, 4));
+
+// Autorole
+const autoroleFile = "./autorole.json";
+if (!fs.existsSync(autoroleFile)) fs.writeFileSync(autoroleFile, "{}");
+const getAutorole = () => {
+    try { return JSON.parse(fs.readFileSync(autoroleFile, "utf8") || "{}"); }
+    catch { return {}; }
+};
+const saveAutorole = (data) => fs.writeFileSync(autoroleFile, JSON.stringify(data, null, 4));
+
+module.exports = { defaultPrefix, getPrefixes, savePrefixes, getBlocked, saveBlocked, getAutorole, saveAutorole };
