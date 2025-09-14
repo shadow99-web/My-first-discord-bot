7661378c21a64e23a394456f5fdfb42d5dae8277
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-14 20:13:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config(); // only needed for local development
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in .env");
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error(
+        "MONGO_URI is not defined. Make sure you set it in .env (local) or in Render environment variables."
+      );
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
