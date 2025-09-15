a73e79d6a811cf7da2b590febd436b0786e3b3ba
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-15 17:52:20 +0530

    Create autoroleHandler.js

---
+++ Handlers/autoroleHandler.js
@@ -0,0 +1,37 @@
+// Handlers/autoroleHandler.js
+const Autorole = require("../Models/autoroleSchema");
+
+async function getAutorole(guildId) {
+    let doc = await Autorole.findOne({ guildId });
+    if (!doc) {
+        doc = new Autorole({ guildId, humans: [], bots: [] });
+        await doc.save();
+    }
+    return doc;
+}
+
+async function addAutorole(guildId, type, roleId) {
+    const doc = await getAutorole(guildId);
+    if (!doc[type].includes(roleId)) {
+        doc[type].push(roleId);
+        await doc.save();
+    }
+    return doc;
+}
+
+async function removeAutorole(guildId, type, roleId) {
+    const doc = await getAutorole(guildId);
+    doc[type] = doc[type].filter(r => r !== roleId);
+    await doc.save();
+    return doc;
+}
+
+async function resetAutorole(guildId) {
+    const doc = await getAutorole(guildId);
+    doc.humans = [];
+    doc.bots = [];
+    await doc.save();
+    return doc;
+}
+
+module.exports = { getAutorole, addAutorole, removeAutorole, resetAutorole };
