523db9a67a3858e92b0f664ed5e3f65397235cdf
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-25 21:11:08 +0530

    Update truthdare.js

--- events/truthdare.js
+++ events/truthdare.js
@@ -4,7 +4,6 @@ module.exports = (client) => {
     client.on("interactionCreate", async (interaction) => {
         if (!interaction.isButton()) return;
 
-        // Only handle truth-dare buttons here
         if (["td_truth", "td_dare", "td_random"].includes(interaction.customId)) {
             await handleTruthDare(interaction);
         }
