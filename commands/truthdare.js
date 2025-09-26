11f970e30d2164d2d595455d0d3a3534580af0bb
shadow99-web <rajendrasingh2320@gmail.com>
2025-09-26 20:55:06 +0530

    Update truthdare.js

--- commands/truthdare.js
+++ commands/truthdare.js
@@ -1,30 +1,44 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("truthdare")
-        .setDescription("Generate a Truth-Dare panel"),
-    async execute({ interaction }) {
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId("td_truth")
-                    .setLabel("Truth")
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId("td_dare")
-                    .setLabel("Dare")
-                    .setStyle(ButtonStyle.Danger),
-                new ButtonBuilder()
-                    .setCustomId("td_random")
-                    .setLabel("Random")
-                    .setStyle(ButtonStyle.Secondary)
-            );
+  data: new SlashCommandBuilder()
+    .setName("truthdare")
+    .setDescription("Generate a Truth-Dare panel"),
+  
+  async execute({ interaction, message, isPrefix, safeReply }) {
+    // Create embed
+    const embed = new EmbedBuilder()
+      .setColor("Purple")
+      .setTitle("🤞🏻 Truth or Dare")
+      .setDescription("Click a button below to choose your challenge!")
+      .setThumbnail(isPrefix ? interaction?.client?.user.displayAvatarURL() : interaction.client.user.displayAvatarURL())
+      .setFooter({
+        text: `Requested by ${isPrefix ? message.author.tag : interaction.user.tag}`,
+        iconURL: isPrefix ? message.author.displayAvatarURL() : interaction.user.displayAvatarURL(),
+      })
+      .setTimestamp();
 
-        await interaction.reply({
-            content: "Choose your challenge:",
-            components: [row],
-            flags: 64 // ephemeral
-        });
+    // Create buttons
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("td_truth")
+        .setLabel("Truth")
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setCustomId("td_dare")
+        .setLabel("Dare")
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId("td_random")
+        .setLabel("Random")
+        .setStyle(ButtonStyle.Secondary)
+    );
+
+    // Send response
+    if (isPrefix && message) {
+      return message.channel.send({ embeds: [embed], components: [row] }).catch(() => {});
+    } else if (interaction && safeReply) {
+      return safeReply({ embeds: [embed], components: [row] });
     }
+  },
 };
