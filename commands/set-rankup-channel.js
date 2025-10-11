e917d81e4f1ab59505919a760a7f0abb09810b18
shadow99-web <rajendrasingh2320@gmail.com>
2025-10-11 17:45:19 +0530

    Create set-rankup-channel.js

---
+++ commands/set-rankup-channel.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+const RankChannel = require("../../models/RankChannel");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("set-rankup-channel")
+    .setDescription("Set the channel where rank-up messages will be sent.")
+    .addChannelOption(option =>
+      option.setName("channel")
+        .setDescription("Select the rank-up channel")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+  async execute({ interaction }) {
+    const channel = interaction.options.getChannel("channel");
+
+    await RankChannel.findOneAndUpdate(
+      { guildId: interaction.guild.id },
+      { channelId: channel.id },
+      { upsert: true, new: true }
+    );
+
+    const embed = new EmbedBuilder()
+      .setColor("Aqua")
+      .setDescription(`✅ Rank-up messages will now appear in ${channel}.`);
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+  },
+};
