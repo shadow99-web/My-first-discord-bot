de14a7c51258cf30d083ef64394952fff1c8b407
shadow99-web <rajendrasingh2320@gmail.com>
2025-10-18 21:34:59 +0530

    Update vcstats.js

--- commands/vcstats.js
+++ commands/vcstats.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const VCStats = require("../schemas/vcStats.js");
+const VCStats = require("..models/vcStatsSchema.js");
 
 module.exports = {
   name: "vcstats",
