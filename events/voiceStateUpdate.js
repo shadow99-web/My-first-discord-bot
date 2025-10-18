74137ccf85c29b585e4b36e908c525a42261dffd
shadow99-web <rajendrasingh2320@gmail.com>
2025-10-18 21:09:28 +0530

    Create voiceStateUpdate.js

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,32 @@
+const VCStats = require("../schemas/vcStats.js");
+
+module.exports = async (client, oldState, newState) => {
+  const user = newState.member?.user || oldState.member?.user;
+  const guild = newState.guild || oldState.guild;
+  if (!user || user.bot) return;
+
+  try {
+    let data = await VCStats.findOne({ userId: user.id, guildId: guild.id });
+    if (!data) {
+      data = await VCStats.create({ userId: user.id, guildId: guild.id });
+    }
+
+    // User JOINED VC
+    if (!oldState.channelId && newState.channelId) {
+      data.lastJoin = Date.now();
+      await data.save();
+    }
+
+    // User LEFT VC
+    else if (oldState.channelId && !newState.channelId) {
+      if (data.lastJoin) {
+        const duration = Date.now() - data.lastJoin;
+        data.totalTime += duration;
+        data.lastJoin = null;
+        await data.save();
+      }
+    }
+  } catch (err) {
+    console.error("VC Tracker Error:", err);
+  }
+};
