b6bd83021bf011bea3fe86f8b0008212aac8ff43
shadow99-web <rajendrasingh2320@gmail.com>
2025-10-22 21:06:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ safeRequireEvent("./events/truthdare", client);
 safeRequireEvent("./events/modLogEvents", client);
 safeRequireEvent("./events/voiceStateUpdate", client);
 safeRequireEvent("./events/antinukeHandler", client);
-safeRequireEvent("./", client);
+safeRequireEvent("./events/reactionRole.js", client);
 // ====================
 // 🚀 Deploy commands and login safely
 async function deployCommandsAndLogin() {
