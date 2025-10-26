7c168e629a2c4efbf673d7744ce3cddb07738f8f
shadow99-web <rajendrasingh2320@gmail.com>
2025-10-26 18:58:46 +0530

    Update WelcomeSettings.js

--- models/WelcomeSettings.js
+++ models/WelcomeSettings.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const WelcomeSettingsSchema = new mongoose.Schema({
   guildId: { type: String, required: true, unique: true },
   channelId: { type: String, required: true },
-  background: { type: String, default: null },
+  background: {
+    type: String,
+    default: "https://i.imgur.com/3ZUrjUP.jpeg", // default background URL
+  },
 });
 
 module.exports = mongoose.model("WelcomeSettings", WelcomeSettingsSchema);
