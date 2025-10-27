2c1b2347020e5c0a5f4328b96a55e23804cb98b8
shadow99-web <rajendrasingh2320@gmail.com>
2025-10-27 21:36:43 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -7,7 +7,8 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const ai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const Level = require("../models/Level");
 const LevelReward = require("../models/LevelReward");
-const { RankCardBuilder } = require("canvacord");
+const { builders } = require("canvacord");
+const { RankCardBuilder } = builders;
 const RankChannel = require("../models/RankChannel");
 const RankSettings = require("../models/RankSettings");
 
@@ -58,7 +59,7 @@ const rankCard = new RankCardBuilder()
   .setRequiredXP(nextLevelXP)
   .setProgressBar({ fill: "#00FFFF", background: "#CCCCCC" })
   .setUsername(message.author.username)
-  .setDiscriminator(message.author.discriminator || "0000")
+  .setDiscriminator(message.author.discriminator ?? "0000")
 
 
 if (background.startsWith("http"))
