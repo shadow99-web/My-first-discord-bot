3ae9fa13bd0254feaef48e681c34d7ebc806fcba
shadow99-web <rajendrasingh2320@gmail.com>
2025-11-21 20:26:10 +0530

    Update AutoPin.js

--- models/AutoPin.js
+++ models/AutoPin.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose");
 
 const AutoPinSchema = new mongoose.Schema({
   guildId: { type: String, required: true },
-  taskId: { type: Number, required: true }, // NEW
+  taskId: { type: Number, required: true },
   channelId: { type: String, required: true },
   query: { type: String, required: true },
   interval: { type: Number, required: true },
   lastPost: { type: Number, default: 0 },
-  lastImage: { type: String, default: "" }, 
+
+  // NEW: store *multiple* previous images
+  postedImages: { type: [String], default: [] },
+
+  // NEW: maximum number of stored images
+  maxHistory: { type: Number, default: 200 }, 
 });
 
 module.exports = mongoose.model("AutoPin", AutoPinSchema);
