0f4e90cb9d3d1e7c32ccd84ab010fae1444776d0
shadow99-web <rajendrasingh2320@gmail.com>
2025-12-30 22:22:14 +0530

    Update truthdare.js

--- commands/truthdare.js
+++ commands/truthdare.js
@@ -1,47 +1,64 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} = require("discord.js");
 
 module.exports = {
+  name: "truthdare",
+
   data: new SlashCommandBuilder()
     .setName("truthdare")
     .setDescription("Generate a Truth-Dare panel"),
 
   async execute({ interaction, message, isPrefix, safeReply }) {
-  const requester = isPrefix ? message.author : interaction.user;
-  const botUser = isPrefix
-    ? message.client.user
-    : interaction.client.user;
+    const requester = isPrefix ? message.author : interaction.user;
+    const botUser = isPrefix
+      ? message.client.user
+      : interaction.client.user;
 
-  const embed = new EmbedBuilder()
-    .setColor("Purple")
-    .setTitle("<:k_:1455575860697497612>  Truth or Dare")
-    .setDescription("Click a button below to choose your challenge!")
-    .setThumbnail(botUser.displayAvatarURL())
-    .setFooter({
-      text: `Requested by ${requester.tag}`,
-      iconURL: requester.displayAvatarURL(),
-    })
-    .setTimestamp();
+    const embed = new EmbedBuilder()
+      .setColor("Purple")
+      .setTitle("<:k_:1455575860697497612> Truth or Dare")
+      .setDescription("Click a button below to choose your challenge!")
+      .setThumbnail(botUser.displayAvatarURL())
+      .setFooter({
+        text: `Requested by ${requester.tag}`,
+        iconURL: requester.displayAvatarURL(),
+      })
+      .setTimestamp();
 
-  const row = new ActionRowBuilder().addComponents(
-    new ButtonBuilder()
-      .setCustomId("td_truth")
-      .setLabel("Truth")
-      .setStyle(ButtonStyle.Primary),
-    new ButtonBuilder()
-      .setCustomId("td_dare")
-      .setLabel("Dare")
-      .setStyle(ButtonStyle.Danger),
-    new ButtonBuilder()
-      .setCustomId("td_random")
-      .setLabel("Random")
-      .setStyle(ButtonStyle.Secondary)
-  );
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("td_truth")
+        .setLabel("Truth")
+        .setStyle(ButtonStyle.Primary),
 
-  if (isPrefix && message) {
-    return message.channel.send({ embeds: [embed], components: [row] });
-  }
+      new ButtonBuilder()
+        .setCustomId("td_dare")
+        .setLabel("Dare")
+        .setStyle(ButtonStyle.Danger),
 
-  return safeReply({ embeds: [embed], components: [row] });
-}
+      new ButtonBuilder()
+        .setCustomId("td_random")
+        .setLabel("Random")
+        .setStyle(ButtonStyle.Secondary)
+    );
+
+    // Prefix command
+    if (isPrefix && message) {
+      return message.channel.send({
+        embeds: [embed],
+        components: [row],
+      });
+    }
+
+    // Slash command
+    return safeReply({
+      embeds: [embed],
+      components: [row],
+    });
   },
 };
