5d879cebcad2a97a514913cc8b69726b9bc99abb
shadow99-web <rajendrasingh2320@gmail.com>
2026-01-06 18:36:22 +0530

    Update stickyHelpers.js

--- utils/stickyHelpers.js
+++ utils/stickyHelpers.js
@@ -1,23 +1,40 @@
 // utils/stickyHelpers.js
 const StickyMessage = require("../models/StickyMessage");
 
+/**
+ * Add or update sticky for a channel
+ */
 async function addSticky({ guildId, channelId, message, authorId }) {
   return StickyMessage.findOneAndUpdate(
     { guildId, channelId },
-    { message, authorId },
+    {
+      guildId,
+      channelId,
+      message,
+      authorId,
+      lastMessageId: null, // 🔥 IMPORTANT: reset old sticky
+    },
     { upsert: true, new: true }
   );
 }
 
+/**
+ * Remove sticky from a channel
+ */
 async function removeSticky({ guildId, channelId }) {
-  return StickyMessage.deleteOne({ guildId, channelId });
+  return StickyMessage.findOneAndDelete({ guildId, channelId });
 }
 
+/**
+ * Get sticky for a channel
+ */
 async function getSticky(channelId) {
   return StickyMessage.findOne({ channelId });
 }
 
-/* ✅ NEW */
+/**
+ * List all stickies in a guild
+ */
 async function listStickies(guildId) {
   return StickyMessage.find({ guildId });
 }
